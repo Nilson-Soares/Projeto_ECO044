108e4a600c8c2de92128d98a230e85bb205b5c3a
pedromirandarf <pedro.miranda@unifei.edu.br>
2017-09-25 11:00:24 -0300

    pedro 1.5 (Menu, Inicio, Abas)

--- app/src/main/java/aplicativo/unifei/edu/br/aplicativo/MainActivity.java
+++ app/src/main/java/aplicativo/unifei/edu/br/aplicativo/MainActivity.java
@@ -1,6 +1,7 @@
 package aplicativo.unifei.edu.br.aplicativo;
 
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -10,29 +11,55 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-            NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-            navigationView.setNavigationItemSelectedListener(this);
-            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-            setSupportActionBar(toolbar);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                    this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-            drawer.setDrawerListener(toggle);
-            toggle.syncState();
-        }
-        @Override
-        public boolean onNavigationItemSelected(MenuItem item) {
-            // Trate os eventos de navegação aqui
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
 
-            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-            drawer.closeDrawer(GravityCompat.START);
+        if (savedInstanceState == null) {
+            navigationView.setCheckedItem(R.id.nav_principal);
+            exibirFragmentPrincipal();
 
-            return true;
         }
     }
+
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Trate os eventos de navegação aqui
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+
+
+
+        switch (item.getItemId()) {
+            case R.id.nav_principal:
+                exibirFragmentPrincipal();
+                break;
+            case R.id.nav_preferencias:
+                // TODO: criar e exibir fragment de preferências
+                break;
+        }
+        return true;
+    }
+    private void exibirFragmentPrincipal() {
+        Fragment novoFragment = PrincipalFragment.newInstance();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.activity_main_conteudo, novoFragment)
+                .commit();
+    }
+
+
+}
---
+++ app/src/main/java/aplicativo/unifei/edu/br/aplicativo/PrincipalFragment.java
@@ -0,0 +1,44 @@
+package aplicativo.unifei.edu.br.aplicativo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PrincipalFragment extends Fragment {
+    public static PrincipalFragment newInstance() {
+        return new PrincipalFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_principal, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TabLayout tabLayout = (TabLayout) view.findViewById(R.id.fragment_principal_tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.Todas));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.Urgentes));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.Normais));
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                // TODO: criar e exibir o fragmento selecionado
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) { }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) { }
+        });
+    }
+}
