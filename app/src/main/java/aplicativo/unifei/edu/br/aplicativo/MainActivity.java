0e2735a5fa72afbb2a2b482fc047b313a8fcd98f
pedromirandarf <pedro.miranda@unifei.edu.br>
2017-09-25 09:46:10 -0300

    Aplicativo com menu e algumas activities

--- app/src/main/java/aplicativo/unifei/edu/br/aplicativo/MainActivity.java
+++ app/src/main/java/aplicativo/unifei/edu/br/aplicativo/MainActivity.java
@@ -1,14 +1,28 @@
 package aplicativo.unifei.edu.br.aplicativo;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+            NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+            navigationView.setNavigationItemSelectedListener(this);
+        }
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item) {
+            // Trate os eventos de navegação aqui
+
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            drawer.closeDrawer(GravityCompat.START);
+
+            return true;
+        }
     }
-}
-etesdgdfsgerdfgdfgfdggreyr
