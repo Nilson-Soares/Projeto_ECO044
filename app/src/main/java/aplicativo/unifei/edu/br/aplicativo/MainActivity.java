d185839fbb8de4b0052088026341bead34925581
pedromirandarf <pedro.miranda@unifei.edu.br>
2017-09-25 09:56:32 -0300

    Aplicativo com menu e algumas activities1.1

--- app/src/main/java/aplicativo/unifei/edu/br/aplicativo/MainActivity.java
+++ app/src/main/java/aplicativo/unifei/edu/br/aplicativo/MainActivity.java
@@ -3,8 +3,10 @@ package aplicativo.unifei.edu.br.aplicativo;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -15,6 +17,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
             NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
             navigationView.setNavigationItemSelectedListener(this);
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                    this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+            drawer.setDrawerListener(toggle);
+            toggle.syncState();
         }
         @Override
         public boolean onNavigationItemSelected(MenuItem item) {
