06bf4df0e45812dcb7eea1d61ffc9c4d7e776a24
Nilson Soares <nilsonsoares2011@unifei.edu.br>
2017-10-16 09:29:17 -0200

    Inclusão do PopUp

--- app/src/main/java/aplicativo/unifei/edu/br/aplicativo/MainActivity.java
+++ app/src/main/java/aplicativo/unifei/edu/br/aplicativo/MainActivity.java
@@ -1,7 +1,10 @@
 package aplicativo.unifei.edu.br.aplicativo;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -9,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     @Override
@@ -18,10 +22,20 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View v) {
+               showEditDialog();
+            }
+        });
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
@@ -80,5 +94,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 .replace(R.id.activity_main_conteudo, novoFragment)
                 .commit();
     }
+
+    private void showEditDialog() {
+        FragmentManager fm = getSupportFragmentManager();
+        PopUp popUp = PopUp.newInstance("Some Title");
+        popUp.show(fm, "fragment_edit_name");
+    }
+
 
 }
---
+++ app/src/main/java/aplicativo/unifei/edu/br/aplicativo/PopUp.java
@@ -0,0 +1,58 @@
+package aplicativo.unifei.edu.br.aplicativo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.EditText;
+
+public class PopUp extends DialogFragment {
+
+    private EditText editText;
+
+    public PopUp() {
+
+    }
+
+    public static PopUp newInstance(String title) {
+        PopUp frag = new PopUp();
+        Bundle args = new Bundle();
+        args.putString("title", title);
+        frag.setArguments(args);
+        return frag;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.popup, container);
+    }
+    @Override
+    public void onResume() {
+        // Get existing layout params for the window
+        ViewGroup.LayoutParams params = getDialog().getWindow().getAttributes();
+        // Assign window properties to fill the parent
+        params.width = WindowManager.LayoutParams.MATCH_PARENT;
+        params.height = WindowManager.LayoutParams.MATCH_PARENT;
+        getDialog().getWindow().setAttributes((android.view.WindowManager.LayoutParams) params);
+        // Call super onResume after sizing
+        super.onResume();
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        // Get field from view
+        editText = (EditText) view.findViewById(R.id.edit_text_popup);
+        // Fetch arguments from bundle and set title
+        String title = getArguments().getString("title", "Enter Name");
+        getDialog().setTitle(title);
+        // Show soft keyboard automatically and request focus to field
+        editText.requestFocus();
+        getDialog().getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+    }
+}
