8b45fafeed217ea72c5e79afdedf31c4efc360cb
pedromirandarf <pedro.miranda@unifei.edu.br>
2017-10-16 11:12:39 -0200

    pedro 2.0 (Melhorias IDV)

--- app/src/main/java/aplicativo/unifei/edu/br/aplicativo/FeitasFragment.java
+++ app/src/main/java/aplicativo/unifei/edu/br/aplicativo/FeitasFragment.java
@@ -18,23 +18,4 @@ public class FeitasFragment extends Fragment {
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_feitas, container, false);
     }
-    @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        TabLayout tabLayout = (TabLayout) view.findViewById(R.id.fragment_feitas_tab_layout);
-        tabLayout.addTab(tabLayout.newTab().setText(R.string.nav_feitas));
-               tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                // TODO: criar e exibir o fragmento selecionado
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) { }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) { }
-        });
-    }
 }
