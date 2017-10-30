654285c4415741428c849bbf1163acd94d3c5a2c
Nilson Soares <nilsonsoares2011@unifei.edu.br>
2017-10-30 09:43:58 -0200

    Criação da classe Tarefa

---
+++ app/src/main/java/aplicativo/unifei/edu/br/aplicativo/Tarefa.java
@@ -0,0 +1,36 @@
+package aplicativo.unifei.edu.br.aplicativo;
+
+/**
+ * Created by Aluno on 30/10/2017.
+ */
+
+public class Tarefa {
+    private String nome;
+    private String descricao;
+
+    public  Tarefa()
+    {
+
+    }
+    public Tarefa(String nome, String descricao)
+    {
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
