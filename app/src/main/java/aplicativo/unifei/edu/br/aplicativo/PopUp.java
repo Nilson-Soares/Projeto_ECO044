d48e942e8fc3c3acd76da03b7add1baff689f734
Nilson Soares <nilsonsoares2011@unifei.edu.br>
2017-11-20 08:42:38 -0200

    Inserção DataPicker e TimePicker

--- app/src/main/java/aplicativo/unifei/edu/br/aplicativo/PopUp.java
+++ app/src/main/java/aplicativo/unifei/edu/br/aplicativo/PopUp.java
@@ -1,6 +1,7 @@
 package aplicativo.unifei.edu.br.aplicativo;
 
-import android.content.Context;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
@@ -10,15 +11,24 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 
-public class PopUp extends DialogFragment {
+import java.util.Calendar;
+
+public class PopUp extends DialogFragment implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
 
     private EditText editTextNome;
     private EditText editTextDescricao;
+    //private EditText data;
+    //private EditText hora;
+    private DatePickerDialog calendario;
+    private TimePickerDialog horario;
+    private Button data;
+    private Button hora;
     private String Nome, Descricao;
-    private Button cancel, salvar;
 
     public PopUp() { }
 
@@ -47,15 +57,27 @@ public class PopUp extends DialogFragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        //Botão cancelar;
+        calendario = new DatePickerDialog(getContext(), PopUp.this, Calendar.getInstance().get(Calendar.YEAR),Calendar.getInstance().get(Calendar.MONTH), Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+        horario = new TimePickerDialog(getContext(), PopUp.this, Calendar.getInstance().get(Calendar.HOUR_OF_DAY), Calendar.getInstance().get(Calendar.MINUTE), true);
+
+        Button cancel;
         cancel = (Button)view.findViewById(R.id.button_cancelar);
-        //Botão salvar
+
+        Button salvar;
         salvar = (Button) view.findViewById(R.id.button_salvar);
 
-        // Nome da tarefa
+        data = (Button) view.findViewById(R.id.buttonData);
+
+        hora = (Button) view.findViewById(R.id.buttonHora);
+
+        // Get field from view
         editTextNome = (EditText) view.findViewById(R.id.edit_text_popup_nome);
-        // Descrição da tarefa
+        // Get field from view
         editTextDescricao = (EditText) view.findViewById(R.id.edit_ext_popup_descricao);
+
+        //editTextDescricao = (EditText) view.findViewById(R.id.editTextData);
+
+        //editTextDescricao = (EditText) view.findViewById(R.id.editTextHora);
 
         // Show soft keyboard automatically and request focus to field
         getDialog().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
@@ -69,6 +91,21 @@ public class PopUp extends DialogFragment {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+
+        data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                calendario.show();
+            }
+        });
+        hora.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                horario.show();
+            }
+        });
 
         cancel.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -87,5 +124,25 @@ public class PopUp extends DialogFragment {
         });
     }
 
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        // store the values selected into a Calendar instance
+        String dataTarefa;
+        final Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, monthOfYear);
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        dataTarefa = c.get(Calendar.DAY_OF_MONTH) + "/" + (c.get(Calendar.MONTH)+1) + "/" + c.get(Calendar.YEAR);
+        data.setText(dataTarefa);
+    }
 
+    @Override
+    public void onTimeSet(TimePicker timePicker, int h, int min) {
+        String horaTarefa;
+        final Calendar c = Calendar.getInstance();
+        c.set(Calendar.HOUR_OF_DAY, h);
+        c.set(Calendar.MINUTE, min);
+        horaTarefa = c.get(Calendar.HOUR_OF_DAY) + ":" + c.get(Calendar.MINUTE);
+        hora.setText(horaTarefa);
+    }
 }
